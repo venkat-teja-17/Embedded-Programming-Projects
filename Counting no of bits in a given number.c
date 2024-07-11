cc13a1dcc6648ab034b95cd2130af3be94df2c7d
MUDE VENKATA TEJA <101350969+venkat-teja-17@users.noreply.github.com>
2024-07-11 13:50:32 +0530

    Update Counting no of bits in a given number.c

--- Counting no of bits in a given number.c
+++ Counting no of bits in a given number.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void binaryToMac(const char* binary) {
-    unsigned int bytes[6] = {0};
-    for (int i = 0; i < 48; ++i) {
-        bytes[i / 8] = (bytes[i / 8] << 1) | (binary[i] - '0');
-    }
-    printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
-           bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5]);
-}
+#define COUNT_SET_BITS(num) ({ int count = 0, n = (num); while (n) { count += n & 1; n >>= 1; } count; })
 
 int main() {
-    char binary_pattern[49];
-    printf("Example of a 48-bit binary pattern: 101010101011101111001100110111011110111111111111\n");
-    printf("Enter a 48-bit binary pattern: ");
-    scanf("%48s", binary_pattern);
-    binaryToMac(binary_pattern);
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    int setBitsCount = COUNT_SET_BITS(number);
+    printf("The number of set bits in the binary representation of %d is: %d\n", number, setBitsCount);
     return 0;
 }
